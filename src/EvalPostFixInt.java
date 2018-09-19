38675da5039c270fa371517375641bbc3bf9912b
StrayBird_ATSH <zhanghuilian-98022@163.com>
2018-09-19 21:22:45 +0800

    The second draft version: Seems nothing needs to be done any more.

---
+++ src/EvalPostFixInt.java
@@ -0,0 +1,89 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+class EvalPostFixInt {
+    /**
+     * @param exp The processed post-fix expression
+     * @return Calculated result of the expression
+     */
+    static double eval(@NotNull String exp) {
+        String doubleOperator = "*/%+-&^|<<>>>";
+        String[] delimitedExp = exp.split(" ");
+        Stack<String> stack = new Stack<>();
+        for (String element : delimitedExp)
+            if (ReversePolishNotation.isOperator(element))
+                if (doubleOperator.contains(element))
+                    stack.push(Integer.toString(evaluateSingle(element,
+                            Integer.parseInt(stack.pop()),
+                            Integer.parseInt(stack.pop()))));
+                else stack.push(Integer.toString(evaluateSingle(element,
+                        Integer.parseInt(stack.pop()))));
+            else if (isNum(element)) stack.push(element);
+            else System.out.println("Invalid expression found at element: " + element);
+        return Integer.parseInt(stack.pop());
+    }
+
+    /**
+     * @param operator Currently processing operator
+     * @param var2     the first popped number
+     * @param var3     the second popped number
+     * @return The result of this single expression
+     */
+    @Contract(pure = true)
+    private static int evaluateSingle(@NotNull String operator, int var2, int var3) {
+        switch (operator) {
+            case "*":
+                return var3 * var2;
+            case "/":
+                return var3 / var2;
+            case "%":
+                return var3 % var2;
+            case "+":
+                return var3 + var2;
+            case "-":
+                return var3 - var2;
+            case "&":
+                return var3 & var2;
+            case "^":
+                return var3 ^ var2;
+            case "|":
+                return var3 | var2;
+            case "<<":
+                return var3 << var2;
+            case ">>":
+                return var3 >> var2;
+            case ">>>":
+                return var3 >>> var2;
+
+        }
+        return 0;
+    }
+
+    /**
+     * @param operator Currently processing operator
+     * @param var2     the first popped number
+     * @return The result of this single expression
+     */
+    @Contract(pure = true)
+    private static int evaluateSingle(@NotNull String operator, int var2) {
+        switch (operator) {
+            case "~":
+                return ~var2;
+        }
+        return 0;
+    }
+
+    /**
+     * Determines whether the element is a number
+     *
+     * @param str the input String
+     * @return true if the String is a number, false otherwise
+     */
+    static private boolean isNum(String str) {
+        String numRegex = "^\\d?$";
+        return Pattern.matches(numRegex, str);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         String expression = ReversePolishNotation.evaluate(
-                "1+(2/{3|2}+1)");
-        System.out.println(EvalPostFix.eval(expression));
-        System.out.println(1 + (2 / (3 | 2) + 1));
+                "1 + cos(sin (2 + 3/(2+5.0)))");
+        try {
+            System.out.println(EvalPostFix.eval(expression));
+//            System.out.println(EvalPostFixInt.eval(expression));
+        } catch (Exception e) {
+            System.out.println("The input expression has semantic errors");
+        }
+        System.out.println(1 + Math.cos(Math.sin(2 + 3 / (2 + 5.0))));
     }
 }
